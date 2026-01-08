92e1ed43d6cb0b7e4d2a868320c1cdc72e958c27
root <dev@01-edu.org>
2026-01-08 10:21:14 +0000

    Add first-move.js

---
+++ first-move.js
@@ -0,0 +1,9 @@
+// Empty file
+// Select the left eye element by ID
+const leftEye = document.getElementById("eye-left");
+
+// Change its background color to black
+leftEye.style.backgroundColor = "black";
+
+// Add the eye-closed class to modify shape
+leftEye.classList.add("eye-closed");
