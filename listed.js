75b21576e370a6e8e08d9c12e67d9749b623d22a
root <dev@01-edu.org>
2026-01-09 10:15:14 +0000

    Add listed.js

---
+++ listed.js
@@ -0,0 +1,6 @@
+// start to code here
+let components = ["motor", "sensor", "battery", "camera"];
+
+let firstPart = robotParts[0];
+let lastPart = robotParts[robotParts.length - 1];
+let comboParts = [lastPart, firstPart];
