f3aae0efa3561d1e9c0e1e91a4243951579f2693
root <dev@01-edu.org>
2026-01-09 11:35:27 +0000

    Add only-if.js

---
+++ only-if.js
@@ -0,0 +1,18 @@
+// start to code here
+// Task 1
+if (truth) {
+  console.log('The truth was spoken.');
+} else {
+  console.log('Lies !!!!');
+}
+
+// Task 2
+ticket = 'You cannot benefit from our special promotion.';
+if (user.age >= 18 && user.age <= 25 && user.activeMembership) {
+  ticket = 'You can benefit from our special promotion.';
+}
+
+// Task 3
+if (customer.cash >= 9.99 || customer.hasVoucher) {
+  ticketSold += 1;
+}
