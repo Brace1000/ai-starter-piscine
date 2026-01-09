1925c15cd1e84c5770916f9e74139321da4d1753
root <dev@01-edu.org>
2026-01-09 10:17:18 +0000

    Update listed.js

--- listed.js
+++ listed.js
@@ -4,3 +4,11 @@ let components = ["motor", "sensor", "battery", "camera"];
 let firstPart = robotParts[0];
 let lastPart = robotParts[robotParts.length - 1];
 let comboParts = [lastPart, firstPart];
+
+// Part A - Replace
+replaceComponents[2] = "enhanced";
+
+// Part B - Swap
+let temp = swapComponents[0];
+swapComponents[0] = swapComponents[1];
+swapComponents[1] = temp;
