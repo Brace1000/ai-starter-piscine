c34d72fdb21d813b531c38fad9effb4d1d69275e
root <dev@01-edu.org>
2026-01-09 12:02:54 +0000

    Add first-function.js

---
+++ first-function.js
@@ -0,0 +1,13 @@
+// start to code here
+// Declare the functions
+let ask = () => {
+  console.log('What is my purpose?');
+};
+
+let reply = () => {
+  console.log('You pass butter.');
+};
+
+// Call the functions
+ask();
+reply();
