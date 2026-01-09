a17598032fce01462050ae5c6f1c28da3b4ea085
root <dev@01-edu.org>
2026-01-09 09:58:35 +0000

    Add transform-objects.js

---
+++ transform-objects.js
@@ -0,0 +1,6 @@
+// start to code here
+robot.model = "RX-78";
+robot.fullName = robot.brand + " " + robot.model;
+robot.batteryLevel = robot.batteryLevel + 10;
+
+let duplicate = `${sentence}, ${sentence}!`;
