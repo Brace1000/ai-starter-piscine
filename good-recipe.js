8a56773d625def6c6a7023591ce6e5fb3f2246a6
root <dev@01-edu.org>
2026-01-09 11:11:24 +0000

    Add good-recipe.js

---
+++ good-recipe.js
@@ -0,0 +1,9 @@
+// start to code here
+let oldestAge = Math.max(martin.age, kevin.age, stephanie.age);
+
+let cutFirst = alphabet.slice(10);
+let cutLast = alphabet.slice(0, -3);
+let cutFirstLast = alphabet.slice(5, -6);
+
+let noCaps = message.toLowerCase();
+let allCaps = message.toUpperCase();
