dce406933439b5dd4dee3e6ced7a16a5ce47060d
root <dev@01-edu.org>
2026-01-09 12:36:20 +0000

    Add i-win-arguments.js

---
+++ i-win-arguments.js
@@ -0,0 +1,23 @@
+// start to code here
+// Task 1
+let battleCry = (message) => {
+  console.log(message.toUpperCase());
+};
+
+let secretOrders = (message) => {
+  console.log(message.toLowerCase());
+};
+
+// Task 2
+let duos = (robot1, robot2) => {
+  console.log(robot1 + ' and ' + robot2 + '!');
+};
+
+let duosWork = (robot1, robot2, task) => {
+  console.log(robot1 + ' and ' + robot2 + ' ' + task + '!');
+};
+
+// Task 3
+let passButter = () => {
+  return 'The butter.';
+};
